2989c0e225cac21863e6cea634fc335b4c5c3146
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-22 20:48:10 +0600

    created a layout for the homepage with bg

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+      <main className='main'>
+        <section className='hero'>
+          <div className='hero__title'>30 Day Test: Day 1</div>
+          <div className='hero__subtitle'>by Earl Nightingle</div>
+        </section>
+
+        <section className='content'>notes will go here</section>
+
+        <section className='sidebar'>video links go here</section>
+
+        <section className='sidebar2'>Another section in handy</section>
+
+        <footer className='footer'>Footer content goes here.</footer>
+      </main>
+    </>
+  )
+}
+
+export default App
