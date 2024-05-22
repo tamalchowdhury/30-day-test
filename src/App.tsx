c6aa10e6c0829a2b221676cd6fb5203586a503c2
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-05-22 21:17:07 +0600

    adds text and headings in the content section

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,47 @@ function App() {
     <>
       <main className='main'>
         <section className='hero'>
-          <div className='hero__title'>30 Day Test: Day 1</div>
+          <div className='hero__title'>30 Day Test</div>
           <div className='hero__subtitle'>by Earl Nightingle</div>
         </section>
 
-        <section className='content'>notes will go here</section>
+        <section className='content'>
+          <h1 className='heading'>
+            <span className='u-bold'>What is Success?</span>{" "}
+            <span className='u-italic'>
+              Success is the progressive realization of a worthy ideal.
+            </span>
+          </h1>
+          <p className='heading'>
+            If you <span className='u-bold'>have a goal</span>, and you are{" "}
+            <span className='u-italic'>working towards it</span>, then{" "}
+            <span className='u-italic'>you are a success.</span>
+          </p>
+          <p className='heading'>
+            One cannot chase two goals at once. Same way a ship cannot reach two
+            ports at once.
+          </p>
+          <p className='heading'>If you ask the captain of a ship:</p>
+          <h1 className='heading u-bold u-italic'>
+            "What's your next destination?"
+          </h1>
+          <p>
+            He/she will immidiately tell you the name of the port they are
+            heading.
+          </p>
+          <h1 className='heading u-bold'>Can you do the same?</h1>
+          <p>
+            Can you immidiately tell me what your next goal is? What do you want
+            to become?
+          </p>
+        </section>
 
-        <section className='sidebar'>video links go here</section>
+        <section className='sidebar'>
+          <div className='sidebar__title'>
+            Earl Nightingle's 30 Day Challenge
+          </div>
+          <div className='sidebar__subtitle'>Watch the Video</div>
+        </section>
 
         <section className='sidebar2'>Another section in handy</section>
 
